15fb4cb5adcb86fd74e4d54b76da05a6ba27d271
Bradley Balchin <bradleybalchin@hotmail.co.uk>
2026-07-25 23:50:31 +0100

    commands from cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Create {
+        name: String,
+    },
+    Delete {
+        id: u32,
+    },
+    List,
+    Save {
+        fname: String,
+    },
+    Load {
+        fname: String
+    },
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Create { name } => println!("Adding {}", name),
+        Commands::Delete { id } => println!("Removing {}", id),
+        Commands::Save { fname } => println!("Removing {}", fname),
+        Commands::Load { fname } => println!("Removing {}", fname),
+        Commands::List => println!("Listing items"),
+    }
 }
