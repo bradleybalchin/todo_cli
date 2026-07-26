40df2acb773b4d2d99132342b0a07320f36a5b9e
Bradley Balchin <bradleybalchin@hotmail.co.uk>
2026-07-26 13:01:04 +0100

    create, delete, reset functionality writing to json

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use clap::{Parser, Subcommand};
 use core::fmt;
-use std::ffi::os_str::Display;
 use std::{env, path::PathBuf};
 use serde::{Deserialize, Serialize};
-use serde_json::Result;
 
 use std::fs::File;
 use std::io::BufWriter;
@@ -21,12 +19,13 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     Create {
-        name: String,
+        name: Vec<String>,
     },
     Delete {
         id: u32,
     },
     List,
+    Reset,
 }
 
 // single todolist item
@@ -41,6 +40,7 @@ struct TodoList(Vec<Item>);
 // custom display format for todolist
 impl  fmt::Display for TodoList {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "------- TODO -------")?;
         for item in &self.0 {
             writeln!(f, "[{}] {}", item.id, item.name)?;
         }
@@ -51,8 +51,30 @@ impl  fmt::Display for TodoList {
 }
 
 
-// initialise todo.json
-fn init_list(){
+// initialise todo.json if does not exist
+fn init_list(path : &PathBuf) -> std::io::Result<()> {
+    let file = File::create(&path)?;
+    let writer = BufWriter::new(file);
+
+    let items:TodoList = TodoList(vec![]);
+    serde_json::to_writer_pretty(writer, &items)?;
+
+    Ok(())
+}
+
+// create a new item and add to list
+fn create(name : String, items : &mut TodoList) {
+    let id  = items.0.len().try_into().unwrap_or(0) + 1;
+    items.0.push(Item { id, name });
+}
+
+// delete item from the list
+fn delete(id : u32, items : &mut TodoList) {
+    items.0.retain(|item| item.id != id);
+
+    for (index, item) in items.0.iter_mut().enumerate() {
+        item.id = (index + 1) as u32;
+    } 
 
 }
 
@@ -63,46 +85,40 @@ fn main() -> std::io::Result<()> {
     let exe_dir = exe.parent().unwrap();
     let path = exe_dir.join(FNAME);
 
+    // empty items list, load in new list from todo.json if exists
+    let mut items:TodoList = TodoList(vec![]);
+
     if path.exists() {
         println!("File exists: {}", path.display());
+        let file = File::open(&path)?;
+        let reader = BufReader::new(file);
+        items = serde_json::from_reader(reader)?;
     } else {
-        println!("File not found {}", path.display());
+        println!("File not found {}, creating todo.json", path.display());
+        init_list(&path)?;
     }
 
+    // Perform Command
     match cli.command {
-        Commands::Create { name } => println!("Adding {}", name),
-        Commands::Delete { id } => println!("Removing {}", id),
-        Commands::List => println!("Listing items {}", FNAME),
+        Commands::Create { name } => {
+            let name = name.join(" ");
+            println!("Adding {}", name);
+            create(name, &mut items)
+        }  ,
+        Commands::Delete { id } => {
+            println!("Removing task {}", id);
+            delete(id, &mut items)
+        },
+        Commands::List => println!("{}", items),
+        Commands::Reset => {
+            println!("Resetting todo.json");
+            items = TodoList(vec![]);
+        }
     }
-
-   
-
-    // creating list, and saving to json example
-    let items = vec![
-        Item {
-            id:1,
-            name: "Lock Front Door".to_string()
-        },
-        Item {
-            id:2,
-            name: "Lock Back Door".to_string()
-        },
-        Item {
-            id:3,
-            name: "Make Dinner".to_string(),
-        },
-    ];
 
     let file = File::create(&path)?;
     let writer = BufWriter::new(file);
     serde_json::to_writer_pretty(writer, &items)?;
-
-    // opening json example
-    let file = File::open(&path)?;
-    let reader = BufReader::new(file);
-
-    let items: TodoList = serde_json::from_reader(reader)?;
-    println!("{}", items);
 
     Ok(())
 
